f9c988b78d53fb8534bc03dcd65c4bf1d34bbee6
Debanjan Guha Thakurata <91559219+debanjandgt@users.noreply.github.com>
2023-08-25 01:38:40 +0530

    Update index.js

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { Button, Form, Input, message } from "antd";
+import React, { useEffect, useState } from "react";
+import { Button, Form, Input, message, notification } from "antd";
 import { Link, useNavigate } from "react-router-dom";
 import Divider from "../../components/Divider";
 import { LoginUser } from "../../apicalls/users";
@@ -13,43 +13,50 @@ const rules = [
     message: "required",
   },
 ];
-function Login() {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
 
-    const [user, setUser] = React.useState({
-        email: '',
-        password: '',
-    })
-    const login = async () => {
-        try {
-            dispatch(ShowLoader());
-            const response = await LoginUser(user);
-            dispatch(HideLoader());
-            if (response.success) {
-                toast.success(response.message);
-                localStorage.setItem('token', response.data);
-                window.location.href = '/';
-            }
-            else {
-                
-                toast.error(response.message);
-              localStorage.removeItem("token");
-              window.location.href = '/login';
-            }
-        }
-        catch (error) {
-            {
-                toast.error(error.message);
-                dispatch(HideLoader());
-            }
-        }
+function Login() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleLogin = async () => {
+    try {
+      dispatch(SetLoader(true));
+      const response = await LoginUser(user);
+      dispatch(SetLoader(false));
+
+      if (response.success) {
+        message.success(response.message);
+        localStorage.setItem("token", response.data);
+        navigate('/');
+      } else {
+        message.error(response.message);
+      }
+    } catch (error) {
+      dispatch(SetLoader(false));
+      message.error(error.message);
     }
-    useEffect(() => {
-        if (localStorage.getItem('token')) {
-            navigate('/');
-        }
-    }, []);
+  };
+
+  useEffect(() => {
+    const existingToken = localStorage.getItem('token');
+
+    if (existingToken) {
+      const decodedToken = jwtDecode(existingToken);
+      const currentTime = Math.floor(Date.now() / 1000);
+
+      if (decodedToken.exp < currentTime) {
+        // Token has expired, remove it
+        localStorage.removeItem('token');
+      } else {
+        // Token is valid, navigate to the home page
+        navigate('/');
+      }
+    }
+  }, []);
 
   return (
     <div className="h-screen bg-cyan-400 flex justify-center items-center">
@@ -58,12 +65,12 @@ function Login() {
           SELL EASY - <span className="text-gray-400 text-2xl">LOGIN</span>
         </h1>
         <Divider />
-        <Form layout="vertical" onFinish={onFinish}>
+        <Form layout="vertical" onFinish={handleLogin}>
           <Form.Item label="Email" name="email" rules={rules}>
-            <Input placeholder="Email" />
+            <Input placeholder="Email" onChange={e => setUser({ ...user, email: e.target.value })} />
           </Form.Item>
           <Form.Item label="Password" name="password" rules={rules}>
-            <Input type="password" placeholder="Password" />
+            <Input type="password" placeholder="Password" onChange={e => setUser({ ...user, password: e.target.value })} />
           </Form.Item>
 
           <Button type="primary" htmlType="submit" block className="mt-2 bg-sky-500 hover:bg-blue-700 ">
